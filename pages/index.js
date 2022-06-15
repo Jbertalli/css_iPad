6aebc6fde94ed405b55609f5a8295f7e33be33e7
Jbertalli <jjbertalli@berkeley.edu>
2022-06-15 13:56:04 -0700

    second

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Back from '../components/ipadBack';
+import Front from '../components/ipadFront';
+
+export default function Home() {
+  return (
+      <>
+        <Head>
+          <title>iPad</title>
+          <meta name="description" content="iPad" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div style={{ transform: 'scale(0.8)' }}>
+          <div style={{ transform: 'translate(-270px, -150px)' }}>
+            <Back />
+          </div>
+          <div style={{ transform: 'translate(30px)' }}>
+            <Front />
+          </div>
+        </div>
+        
+      </>
+    );
+}
