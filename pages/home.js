90cb15863250a4a922738bb557824ec4e9b359b5
Jbertalli <jjbertalli@berkeley.edu>
2022-09-01 12:43:00 -0700

    Accordion component + Head

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,7 @@
-import { Grid } from 'semantic-ui-react';
-import Accordion from '../ipad_accordion';
 import { useEffect } from 'react';
+import Head from 'next/head';
+import { Grid } from 'semantic-ui-react';
+import Accordion from '../components/ipad_accordion';
 
 export default function Home () {
 
@@ -13,6 +14,10 @@ export default function Home () {
     
     return (
         <>
+            <Head>
+                <title>iPad Accordion - Apple</title>
+                <meta name="description" content="apple, ipad" />
+            </Head>
             <div>
                 <Grid style={{ display: 'flex', justifyContent: 'center', transform: 'translate(107px)', position: 'fixed' }}>
                     <Grid.Column style={{ width: '450px', transform: 'translateX(541px)' }}>
