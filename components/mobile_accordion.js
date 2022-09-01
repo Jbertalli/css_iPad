c2f02ce8d313b145dca074b3631bbfe279ce2311
Jbertalli <jjbertalli@berkeley.edu>
2022-09-01 13:19:40 -0700

    mobile responsive accordion

--- components/mobile_accordion.js
+++ components/mobile_accordion.js
@@ -3,8 +3,8 @@ import Accordion from "./ipad_accordion";
 export default function Mobile_Accordion() {
 
   return (
-      <>
+      <div style={{ transform: 'translate(700px, 1500px) scale(1.5)', display: 'flex', justifyContent: 'center' }}>
         <Accordion />
-      </>
+      </div>
     );
 }
--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,29 @@
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { Grid } from 'semantic-ui-react';
 import Accordion from '../components/ipad_accordion';
+import Mobile_Accordion from '../components/mobile_accordion';
 
 export default function Home () {
+    const [isDesktop, setIsDesktop] = useState(false);
+ 
+    useEffect(() => {
+        if (window.innerWidth > 440) {
+            setIsDesktop(true);
+        } else {
+            setIsDesktop(false);
+        }
+
+        const updateMedia = () => {
+        if (window.innerWidth > 440) {
+            setIsDesktop(true);
+        } else {
+            setIsDesktop(false);
+        }
+        };
+        window.addEventListener('resize', updateMedia);
+        return () => window.removeEventListener('resize', updateMedia);
+    }, []);
 
     useEffect(() => {
         document.body.style.overflowX = "hidden";
@@ -18,13 +38,21 @@ export default function Home () {
                 <title>iPad Accordion - Apple</title>
                 <meta name="description" content="apple, ipad" />
             </Head>
-            <div style={{ display: 'flex', justifyContent: 'center', position: 'fixed' }}>
-                <Grid>
-                    <Grid.Column style={{ width: '450px', transform: 'translate(1300px, 4600px) scale(2.7)' }}>
-                        <Accordion />
-                    </Grid.Column>
-                </Grid>
-            </div>
+            {isDesktop ? (
+            <>
+                <div style={{ display: 'flex', justifyContent: 'center', position: 'fixed' }}>
+                    <Grid>
+                        <Grid.Column style={{ width: '450px', transform: 'translate(1300px, 4600px) scale(2.7)' }}>
+                            <Accordion />
+                        </Grid.Column>
+                    </Grid>
+                </div>
+            </>
+            ):(
+            <>
+                <Mobile_Accordion />
+            </>
+            )}
         </>
     );
 }
